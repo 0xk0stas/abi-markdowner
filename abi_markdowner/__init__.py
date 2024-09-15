8331c85d88e6e3343bf2c853e2c7cdcc9c67eea8
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-09-15 19:18:10 +0300

    keep trying

--- abi_markdowner/__init__.py
+++ abi_markdowner/__init__.py
@@ -1,5 +1,10 @@
 # __init__.py
 
 __version__ = "0.1.1"
-__author__ = "Your Name"
+__author__ = "Kostas Tzoumpas"
 __license__ = "MIT"
+
+from .abi_markdowner import generate_markdown_from_abi
+from .file_io import save_markdown_to_file, read_abi_from_file
+
+__all__ = ["generate_markdown_from_abi", "save_markdown_to_file", "read_abi_from_file"]
